99b4f57e4b1b41fe39c168f1242c5b8736056e0e
David Freitas <dvdfreitas@gmail.com>
2024-10-16 16:24:16 +0100

    first commit

---
+++ resources/views/parceiros.blade.php
@@ -0,0 +1,19 @@
+<x-guestLayout>
+    <div class="max-w-7xl m-auto">
+        <p class="text-7xl text-red-500 text-center my-16">Se faz sentir, faz sentido!</p>
+        <div class="max-w-5xl m-auto">
+            <iframe width="560" height="315" class="m-auto" src="https://www.youtube.com/embed/wW4-RXkAeDM?si=dqhvVwvgw2cSMLlo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <p class="text-3xl my-8 m-auto text-center">Na corrida e na vida, ninguém fica para trás!</p>
+        </div>
+        <div class="max-w-5xl m-auto space-y-4">
+            <p>A Academia Sexto Sentido promove a inclusão através do desporto, capacitando todos os envolvidos para criar ambientes inclusivos e oportunidades iguais.</p>
+            <p>Oferece a pessoas cegas e com baixa visão uma equipa de marcha e corrida guiada, melhorando a qualidade de vida e bem-estar.</p>
+            <p>Além disso, capacita empresas com consultoria, teambuilding e formação especializada, a integrar pessoas com deficiência e a promover ambientes detrabalho acolhedores e o crescimento profissional de todos. </p>
+        </div>
+        <div class="grid grid-cols-3 h-16 gap-3 max-w-lg m-auto my-8 text-center">
+            <a href="" class="hover:bg-red-500 bg-red-400 rounded">Participa</a>
+            <a href="" class="hover:bg-purple-500 bg-purple-400 rounded">Serviços</a>
+            <a href="" class="hover:bg-indigo-500 bg-indigo-400 rounded">Agenda</a>
+        </div>
+    </div>
+</x-guestLayout>
