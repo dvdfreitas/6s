87d593b425b801ddac006ce6f79b8e5702c9588a
dvdfreitas <dvdfreitas@gmail.com>
2024-11-04 12:52:52 +0000

    241104 12:52

---
+++ resources/views/preview.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="shortcut icon" type="image/x-icon" href="/images/logo_color.svg" />
+
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+
+        <!-- Flowbite - HLD -->
+        <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    </head>
+    <body class="m-8">
+        <img src="/images/logo_color.svg" class="max-w-xl m-auto" alt="Sexto Sentido Logo" />
+        <div class="m-auto text-center text-[#FF2D20] font-extralight">
+            <p class="text-7xl">Em construção</p>
+            <p class="text-3xl">Novidades em breve</p>
+        </div>
+        @livewireScripts
+        <!-- Flowbite - HLD -->
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+    </body>
+</html>
